e871c55e2a2e7f0290543ea1aab6b96ac902ea2e
sergioandresco <sergiocobos6@outlook.com>
2024-09-30 22:07:16 -0500

    change logic

--- src/utils/compressImageLocal.js
+++ src/utils/compressImageLocal.js
@@ -1,13 +1,7 @@
 // src/compressLocal.js
 import imageCompression from 'browser-image-compression';
 
-const validImageTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp', 'image/tiff', 'image/avif', 'image/svg+xml'];
-
-export const compressLocalImage = async (file, maxQuality = 80) => {
-  if (!validImageTypes.includes(file.type)) {
-    throw new Error('Tipo de archivo no soportado. Acepta JPEG, JPG, PNG, WEBP, TIFF, AVIF y SVG.');
-  }
-
+export const compressLocalImage = async (files, maxQuality = 80) => {
   const options = {
     maxSizeMB: 1, // Puedes ajustar esto según tus necesidades
     useWebWorker: true,
@@ -15,10 +9,23 @@ export const compressLocalImage = async (file, maxQuality = 80) => {
   };
 
   try {
-    const compressedFile = await imageCompression(file, options);
-    return compressedFile;
+    // Si es un solo archivo, lo manejamos directamente
+    if (!Array.isArray(files)) {
+      const compressedFile = await imageCompression(files, options);
+      return compressedFile;
+    }
+
+    // Si es un array de archivos, comprimimos cada uno
+    const compressedFiles = await Promise.all(
+      files.map(async (file) => {
+        const compressedFile = await imageCompression(file, options);
+        return compressedFile;
+      })
+    );
+    
+    return compressedFiles; // Devuelve un array de archivos comprimidos
   } catch (error) {
-    console.error('Error al comprimir la imagen local:', error);
+    console.error('Error al comprimir las imágenes locales:', error);
     throw error;
   }
 };
